0f011503bbded31894aa4d36a950e66203f16532
Noj Vek <nojvek@gmail.com>
2021-05-25 21:29:22 -0700

    parse and stringify working

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,68 +5,83 @@ global.TextEncoder = require('util').TextEncoder;
 global.TextDecoder = require('util').TextDecoder;
 
 interface StringifyTest {
+  name?: string;
   val: any;
   expect: string;
 }
 
 describe('urltron', () => {
-  describe('stringify', () => {
-    const tests: StringifyTest[] = [
-      // strings
-      {val: '', expect: "'"},
-      {val: 'hello', expect: 'hello'},
-      {
-        val: '~!@#$%^&*_+-=(){}[]<>|\\/? "\'',
-        expect: '%7E%21%40%23$%25%5E%26%2A_%2B-%3D%28%29%7B%7D%5B%5D%3C%3E%7C%5C%2F%3F%20%22%27',
+  const tests: StringifyTest[] = [
+    // strings
+    {val: '', expect: "'"},
+    {val: 'hello', expect: 'hello'},
+    {
+      val: '~!@#$%^&*_+-=(){}[]<>|\\/? "\'',
+      expect: '%7E%21%40%23$%25%5E%26%2A_%2B-%3D%28%29%7B%7D%5B%5D%3C%3E%7C%5C%2F%3F%20%22%27',
+    },
+    {val: 'http://example.com/a b.jpg', expect: 'http%3A%2F%2Fexample.com%2Fa%20b.jpg'},
+    {val: '😀', expect: '%F0%9F%98%80'},
+    {val: 't', expect: "'t"},
+    {val: 'f', expect: "'f"},
+    {val: 'n', expect: "'n"},
+    {val: 'true', expect: 'true'},
+    {val: 'false', expect: 'false'},
+    {val: '1.2', expect: "'1.2"},
+    {val: '0', expect: "'0"},
+
+    {val: true, expect: 't'},
+    {val: false, expect: 'f'},
+    {val: null, expect: 'n'},
+
+    {
+      name: 'simple array',
+      val: [true, false, null, 'hello world', 1.234],
+      expect: '@(t,f,n,hello%20world,1.234)',
+    },
+
+    {name: 'empty object', val: {}, expect: ''},
+    {
+      name: 'simple object',
+      val: {limit: 10, offset: 20, query: 'hello world', notyet: undefined},
+      expect: 'limit=10&offset=20&query=hello%20world',
+    },
+    {
+      name: 'complex object',
+      val: {
+        select: ['id', 'name', 'age'],
+        from: {table: 'users'},
+        where: [
+          {field: 'name', op: '%', val: 'foo'},
+          {field: 'age', op: '>', val: 20},
+        ],
+        fullTable: true,
       },
-      {val: 'http://example.com/a b.jpg', expect: 'http%3A%2F%2Fexample.com%2Fa%20b.jpg'},
-      {val: '😀', expect: '%F0%9F%98%80'},
-      {val: 't', expect: "'t"},
-      {val: 'f', expect: "'f"},
-      {val: 'n', expect: "'n"},
-      {val: 'true', expect: 'true'},
-      {val: 'false', expect: 'false'},
-      {val: '1.2', expect: "'1.2"},
-      {val: '0', expect: "'0"},
+      expect:
+        'select=@(id,name,age)&from=(table=users)&where=@((field=name&op=%25&val=foo),(field=age&op=%3E&val=20))&fullTable=t',
+    },
 
-      // true, false, null
-      {val: true, expect: 't'},
-      {val: false, expect: 'f'},
-      {val: null, expect: 'n'},
-
-      // object
-      {
-        val: {limit: 10, offset: 20, query: 'hello world', notyet: undefined},
-        expect: 'limit=10&offset=20&query=hello%20world',
-      },
-      {val: {}, expect: ''},
-
-      // array
-      {
-        val: [true, false, null, 'hello world', 1.234],
-        expect: '@(t,f,n,hello%20world,1.234)',
-      },
-
-      // nested complex object
-      {
-        val: {
-          select: ['id', 'name', 'age'],
-          from: {table: 'users'},
-          where: [
-            {field: 'name', op: '%', val: 'foo'},
-            {field: 'age', op: '>', val: 20},
-          ],
-          fullTable: true,
+    {
+      name: 'complex escaped object',
+      val: {
+        '~!@#$%^&*()=%': {
+          num: [1, 2.3, -3e100, true, false, null, undefined, NaN],
+          str: ['1', '2', '3', 't', 'f', 'n'],
+          '': '',
         },
-        expect:
-          'select=@(id,name,age)&from=(table=users)&where=@((field=name&op=%25&val=foo),(field=age&op=%3E&val=20))&fullTable=t',
       },
-    ];
+      expect: "%7E%21%40%23$%25%5E%26%2A%28%29%3D%25=(num=@(1,2.3,-3e+100,t,f,n,n,n)&str=@('1,'2,'3,'t,'f,'n)&'=')",
+    },
+  ];
 
-    for (const test of tests) {
-      it(JSON.stringify(test.val), () => {
-        expect(_stringify(test.val, 0)).toEqual(test.expect);
-      });
-    }
-  });
+  for (const test of tests) {
+    it(test.name || JSON.stringify(test.val), () => {
+      const str = _stringify(test.val, 0);
+      expect(str).toEqual(test.expect);
+
+      if (typeof test.val === 'object' && test.val) {
+        // ensure parse(stringify) == val (with undefined keys deep removed)
+        expect(JSON.stringify(parse(str))).toEqual(JSON.stringify(test.val));
+      }
+    });
+  }
 });
--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,8 @@ function escapeNonWordChars(val: string): string {
  */
 export function _stringify(val: any, depth: number): string {
   const valType = typeof val;
-  if (val === null) {
+  // JSON.stringify converts undefined and NaN to null
+  if (val === null || val === undefined || Number.isNaN(val)) {
     return 'n';
   } else if (valType === 'boolean') {
     return val ? 't' : 'f';
@@ -60,9 +61,108 @@ export function stringify(val: any): string {
   return _stringify(val, 0);
 }
 
+interface Lexer {
+  tokens: string[];
+  peek: () => string | null;
+  next: () => boolean;
+}
+
+function _lex(str: string): Lexer {
+  if (!str || (str[0] != '@' && str[0] != '(')) {
+    str = `(${str})`; // assume parsing object by default
+  }
+  // split string while keeping delimiters
+  // @see https://medium.com/@shemar.gordon32/how-to-split-and-keep-the-delimiter-s-d433fb697c65
+  const tokens = str.split(/(?=[@()=&,])|(?<=[@()=&,])/g);
+  let idx = 0;
+  return {
+    tokens,
+    peek: () => (idx < tokens.length ? tokens[idx] : null),
+    next: () => ((idx += 1), idx < tokens.length),
+  };
+}
+
+function _ensureToken(lexer: Lexer, expected: string, consume = true) {
+  if (lexer.peek() !== expected) {
+    throw new Error(`urltron.parse: expecting:'${expected}', found:'${lexer.peek()}'`);
+  }
+  if (consume) {
+    lexer.next();
+  }
+}
+
+function _parseObject(lexer: Lexer): Record<string, any> {
+  const obj: Record<string, any> = {};
+
+  _ensureToken(lexer, '(');
+  while (lexer.peek() !== ')') {
+    const key = _parseString(lexer);
+    _ensureToken(lexer, '=');
+    const val = _parseValue(lexer);
+    if (lexer.peek() !== ')') {
+      _ensureToken(lexer, '&');
+    }
+    obj[key] = val;
+  }
+  _ensureToken(lexer, ')');
+
+  return obj;
+}
+
+function _parseArray(lexer: Lexer): any[] {
+  const arr: any[] = [];
+
+  _ensureToken(lexer, '@');
+  _ensureToken(lexer, '(');
+  while (lexer.peek() !== ')') {
+    const val = _parseValue(lexer);
+    if (lexer.peek() !== ')') {
+      _ensureToken(lexer, ',');
+    }
+    arr.push(val);
+  }
+  _ensureToken(lexer, ')');
+
+  return arr;
+}
+
+function _parseString(lexer: Lexer): string {
+  let str = lexer.peek()!;
+  lexer.next();
+  if (str[0] === "'") {
+    str = str.slice(1);
+  }
+  return decodeURIComponent(str);
+}
+
+function _parseValue(lexer: Lexer): any {
+  const curToken = lexer.peek();
+  if (!curToken) {
+    throw new Error(`urltron.parse: invalid token:'${curToken}'`);
+  } else if (curToken == '(') {
+    return _parseObject(lexer);
+  } else if (curToken == '@') {
+    return _parseArray(lexer);
+  } else if (curToken == 't') {
+    lexer.next();
+    return true;
+  } else if (curToken == 'f') {
+    lexer.next();
+    return false;
+  } else if (curToken == 'n') {
+    lexer.next();
+    return null;
+  } else if (/^-?[0-9]/.test(curToken)) {
+    lexer.next();
+    return parseFloat(curToken);
+  } else {
+    return _parseString(lexer);
+  }
+}
+
 /**
  * Parse object or array from query params-ish string
  */
 export function parse(str: string): any {
-  return str;
+  return _parseValue(_lex(str));
 }
